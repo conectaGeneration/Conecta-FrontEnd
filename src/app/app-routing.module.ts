f85df3effce859aa78698adbbc7ecfc2f209b0f2
luis fernando <fer.almeidabr@hotmail.com>
2021-12-06 11:50:21 -0300

    aplicando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+// import { pathToFileURL } from 'url';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path:'', redirectTo: 'sobre', pathMatch: 'full'},
+  {
+    path:'menu', component: MenuComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
