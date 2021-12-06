8db33efad7530a1cf78f37b160cfc937f8f19c76
luis fernando <fer.almeidabr@hotmail.com>
2021-12-06 10:39:31 -0300

    component sobre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { MenuComponent } from './menu/menu.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SobreComponent } from './sobre/sobre.component';
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+// ESTA COM  A IMPORTAÇÃO DO FONTAWESOME
 
 @NgModule({
   declarations: [
     AppComponent,
     RodapeComponent,
-    MenuComponent
+    MenuComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    // FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
