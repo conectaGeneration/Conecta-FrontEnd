e6f3461ba6b0829755cae6796aaaa7ef2e0c93f6
helensouz <sousahelen816@gmail.com>
2021-12-14 16:28:24 -0300

    get-post-tema consumo

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,9 @@
+import { TemaService } from './../service/tema.service';
+import { Tema } from './../model/Tema';
 import { environment } from 'src/environments/environment.prod';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Route } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-tema',
@@ -9,14 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+
+
+
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService,
   ) { }
 
   ngOnInit(){
     if(environment.token == ''){
       this.router.navigate(['/entrar'])
     }
+  }
+
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.tema = new Tema()
+    })
   }
 
 }
